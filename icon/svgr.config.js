694b97c3f8b228fe12c9b68a5123c016fd2a41b1
huyng12 <touch@nguyenrk.com>
2021-06-17 02:20:33 +0700

    Add Star Solid icon

--- icon/svgr.config.js
+++ icon/svgr.config.js
@@ -4,7 +4,8 @@ module.exports = {
   // The way to set the color of the svg instead of fix "fill/stroke" attribute
   // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#currentcolor_keyword
   replaceAttrValues: {
-    '#333C44': 'currentColor'
+    '#333C44': 'currentColor',
+    '#FFD66C': 'currentColor'
   },
   icon: true,
   titleProp: false,
