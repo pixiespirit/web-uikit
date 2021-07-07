af1fc1f6279b8dd5ccf240503c448f43b40c0096
huyng12 <touch@nguyenrk.com>
2021-07-08 00:30:28 +0700

    Setup ESLint and Prettier check

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+    // 'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // eslint currently not detect props typing definitions
+    // as a props validator
+    // https://github.com/yannickcr/eslint-plugin-react/issues/2353
+    'react/prop-types': 'off',
+
+    // TODO: Turn it on again and fix errors
+    'react/display-name': 'off',
+
+    // I prefers pass children as prop if it is a single ReactNode child
+    // (no-nested-child)
+    'react/no-children-prop': 'off',
+
+    // 'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+
+    // Eslint false positive: Consider enum as unused variables
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2621
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    // https://reactjs.org/docs/hooks-rules.html
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
+  },
+  settings: {
+    // Warning: React version not specified in eslint-plugin-react settings
+    // https://github.com/yannickcr/eslint-plugin-react/issues/1955
+    react: {
+      version: 'detect'
+    }
+  },
+  globals: {
+    JSX: 'readonly'
+  }
+};
