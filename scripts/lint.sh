2b645665d032828a24e1875c5807343306430f3b
huyng12 <touch@nguyenrk.com>
2021-07-09 02:56:46 +0700

    Update lint, format and prettier check

---
+++ scripts/lint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# lint check core
+yarn eslint ./core/src \
+    --max-warnings=0 \
+    --ext=.jsx,.js,.ts,.tsx \
+    --ignore-path=./core/.gitignore
+
+# lint check icon
+yarn eslint ./icon/src \
+    --max-warnings=0 \
+    --ext=.jsx,.js,.ts,.tsx \
+    --ignore-path=./icon/.gitignore
+
+# lint check site
+cd site && yarn lint
---
+++ scripts/prettier.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+yarn prettier --write --loglevel=silent ./core/src
+yarn prettier --write --loglevel=silent ./icon/src
+yarn prettier --write --loglevel=silent ./site/pages
