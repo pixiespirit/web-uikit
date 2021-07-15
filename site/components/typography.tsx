323013623e64ded1745d349741a62d333280782b
huyng12 <touch@nguyenrk.com>
2021-07-15 10:56:28 +0700

    Update label4 line-height

--- site/components/typography.tsx
+++ site/components/typography.tsx
@@ -75,19 +75,19 @@ export const GalleryTypography = (): JSX.Element => {
 
         <Label1 semiBold>Label 01</Label1>
         <Label1 medium>Label 01</Label1>
-        <Label1>Label 01</Label1>
+        <Label1>Paragraph 01</Label1>
 
         <Label2 semiBold>Label 02</Label2>
         <Label2 medium>Label 02</Label2>
-        <Label2>Label 02</Label2>
+        <Label2>Paragraph 02</Label2>
 
         <Label3 semiBold>Label 03</Label3>
         <Label3 medium>Label 03</Label3>
-        <Label3>Label 03</Label3>
+        <Label3>Paragraph 03</Label3>
 
         <Label4 semiBold>Label 04</Label4>
         <Label4 medium>Label 04</Label4>
-        <Label4>Label 04</Label4>
+        <Label4>Paragraph 04</Label4>
       </div>
     </div>
   );
