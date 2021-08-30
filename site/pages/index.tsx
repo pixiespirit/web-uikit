9547344cb193212827e2b719493b29ed8449f1ff
mtosity <mtosity@gmail.com>
2021-08-31 02:36:37 +0700

    feat: add input-demo

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,13 +1,7 @@
 import Head from 'next/head';
 import React, { CSSProperties } from 'react';
-import {
-  Input,
-  InputGroup,
-  InputLeftIcon,
-  InputRightIcon
-} from '../../core/dist';
-import { ImagePicture2Outline } from '@pixiepkg/icon';
 import { ButtonDemo } from '../components/button-demo';
+import { InputDemo } from '../components/input-demo';
 
 const style: CSSProperties = {
   maxWidth: 1170,
@@ -30,41 +24,7 @@ export default function Home() {
       </Head>
 
       <main style={style}>
-        <div className="wrapper">
-          <h2 className="title">Input</h2>
-          <div className="mb-4">
-            <Input placeholder={'Sizes prop sm'} sizes={'sm'} />
-          </div>
-          <div className="mb-4">
-            <Input placeholder={'Sizes prop md as default'} />
-          </div>
-          <div className="mb-4">
-            <Input placeholder={'sizes props lg'} sizes={'lg'} />
-          </div>
-
-          <div className="mb-4">
-            <Input placeholder={'Invalid input'} required />
-          </div>
-
-          <div className="mb-4">
-            <InputGroup>
-              {/*<InputRightIcon children={<span>haha</span>}/>*/}
-              <Input placeholder={'Left icon with action onClick'} />
-              <InputLeftIcon onClick={() => alert('ok')}>
-                <ImagePicture2Outline size={24} display={'block'} />
-              </InputLeftIcon>
-            </InputGroup>
-          </div>
-          <div className="mb-4">
-            <InputGroup>
-              <Input placeholder={'Right icon'} />
-              <InputRightIcon>
-                <ImagePicture2Outline size={24} display={'block'} />
-              </InputRightIcon>
-            </InputGroup>
-          </div>
-        </div>
-
+        <InputDemo />
         <ButtonDemo />
         {/*<GalleryTypographyIntro />*/}
         {/*<GalleryButton />*/}
