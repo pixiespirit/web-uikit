a6c04b4a016ae5e9472ac90fdf64b43ef613702a
hidang <yellowstudio.vn@gmail.com>
2021-09-03 20:56:45 +0700

    fix lint

--- core/src/textarea/types.ts
+++ core/src/textarea/types.ts
@@ -1,52 +1,51 @@
 import {
-  WeakValidationMap,
-  ValidationMap,
-  PropsWithChildren,
-  ReactElement,
-  CSSProperties,
-  TextareaHTMLAttributes
+    WeakValidationMap,
+    ValidationMap,
+    PropsWithChildren,
+    ReactElement,
+    CSSProperties,
+    TextareaHTMLAttributes
 } from 'react';
 
-export interface TextareaProps
-  extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-  defaultValue?: string | number;
-  forwardedRef?: React.ForwardedRef<HTMLTextAreaElement>;
-  value?: string;
-  setValue?: (value: string) => void;
+export interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    defaultValue?: string | number;
+    forwardedRef?: React.ForwardedRef<HTMLTextAreaElement>;
+    value?: string;
+    setValue?: (value: string) => void;
 
-  // Attributes
-  id?: string;
-  label?: string;
-  caption?: string;
-  autoComplete?: string;
-  autoFocus?: boolean;
-  cols?: number;
-  rows?: number;
-  disabled?: boolean;
-  form?: string;
-  'aria-label'?: string;
-  'aria-labelledby'?: string;
-  maxLength?: number;
-  minLength?: number;
-  name?: string;
-  required?: boolean;
-  readonly?: boolean;
-  spellCheck?: boolean;
-  wrap?: string;
-  invalid?: boolean;
+    // Attributes
+    id?: string;
+    label?: string;
+    caption?: string;
+    autoComplete?: string;
+    autoFocus?: boolean;
+    cols?: number;
+    rows?: number;
+    disabled?: boolean;
+    form?: string;
+    'aria-label'?: string;
+    'aria-labelledby'?: string;
+    maxLength?: number;
+    minLength?: number;
+    name?: string;
+    required?: boolean;
+    readonly?: boolean;
+    spellCheck?: boolean;
+    wrap?: string;
+    invalid?: boolean;
 
-  // Styling
-  className?: string;
-  style?: CSSProperties;
+    // Styling
+    className?: string;
+    style?: CSSProperties;
 }
 
 interface FunctionComponent<P = Record<string, unknown>> {
-  (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
-  propTypes?: WeakValidationMap<P>;
-  contextTypes?: ValidationMap<any>;
-  defaultProps?: Partial<P>;
-  displayName?: string;
-  id?: string;
+    (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
+    propTypes?: WeakValidationMap<P>;
+    contextTypes?: ValidationMap<any>;
+    defaultProps?: Partial<P>;
+    displayName?: string;
+    id?: string;
 }
 
 export type SFC<P = Record<string, any>> = FunctionComponent<P>;
