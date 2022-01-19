c7edc4fb6f73a2b92141d81364d121a5ae9c5dc4
mtosity <mtosity@gmail.com>
2022-01-19 14:49:26 +0700

    update webisite demo pixie logo

--- demo/react-site/src/App.tsx
+++ demo/react-site/src/App.tsx
@@ -8,6 +8,8 @@ import { InputDemo } from "./components/input-demo";
 import { TextAreaDemo } from "./components/text-area-demo";
 import { ToggleDemo } from "./components/toggle-demo";
 import { TypographyDemo } from "./components/typography-demo";
+import { Label2 } from "@pixiepkg/react";
+import { PixieSolid } from "@pixiepkg/icon";
 
 const style: CSSProperties = {
   maxWidth: 1170,
@@ -20,12 +22,23 @@ const style: CSSProperties = {
 function App() {
   return (
     <div style={style}>
+      <a
+        href="https://gotpixie.com/"
+        style={{ marginLeft: 20 }}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <PixieSolid size={120} />
+      </a>
       <TypographyDemo />
       <InputDemo />
       <ButtonDemo />
       <TextAreaDemo />
       <CheckBoxDemo />
       <ToggleDemo />
+      <div>
+        <Label2>{"©️ 2021 Pixie - All Rights Reserved."}</Label2>
+      </div>
     </div>
   );
 }
