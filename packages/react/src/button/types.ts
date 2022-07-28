c0b5a4d71b5a139cde5699cb00b0669d97ab447b
tri.vo <tri.vo@mservice.com.vn>
2022-07-29 01:43:00 +0700

    button loading

--- packages/react/src/button/types.ts
+++ packages/react/src/button/types.ts
@@ -4,6 +4,7 @@ import { DivSize } from '@/div';
 export interface SizeButtonType {
     root: string;
     iconMargin: DivSize;
+    x_key: SizeButtonKey;
 }
 
 export type SizeButtonKey = 'SMALL' | 'MEDIUM' | 'LARGE' | 'XLARGE';
@@ -11,19 +12,23 @@ export type SizeButtonKey = 'SMALL' | 'MEDIUM' | 'LARGE' | 'XLARGE';
 export const SizeButton: Record<SizeButtonKey, SizeButtonType> = {
     SMALL: {
         root: size.small,
-        iconMargin: 4
+        iconMargin: 4,
+        x_key: 'SMALL'
     },
     MEDIUM: {
         root: size.medium,
-        iconMargin: 8
+        iconMargin: 8,
+        x_key: 'MEDIUM'
     },
     LARGE: {
         root: size.large,
-        iconMargin: 8
+        iconMargin: 8,
+        x_key: 'LARGE'
     },
     XLARGE: {
         root: size.xlarge,
-        iconMargin: 8
+        iconMargin: 8,
+        x_key: 'XLARGE'
     }
 };
 
